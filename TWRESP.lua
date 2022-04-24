633441bfe1e58140de0ad9691219ce4ca9284ec6
eya <66527398+botslaye@users.noreply.github.com>
2022-04-24 14:45:56 -0400

    Added objective ESP

--- TWRESP.lua
+++ TWRESP.lua
@@ -2,11 +2,19 @@ local library = loadstring(game:HttpGet('https://raw.githubusercontent.com/obese
 
 local ESP = loadstring(game:HttpGet("https://raw.githubusercontent.com/wally-rblx/ESP-Lib/main/ESP.lua"))()
 local ESP2 = loadstring(game:HttpGet("https://raw.githubusercontent.com/wally-rblx/ESP-Lib/main/ESP.lua"))()
+local ESP3 = loadstring(game:HttpGet("https://raw.githubusercontent.com/wally-rblx/ESP-Lib/main/ESP.lua"))()
 ESP:Toggle(true); ESP.Players = false; ESP.Color = Color3.fromRGB(128,234,255)
 ESP2:Toggle(true); ESP2.Boxes = false; ESP2.Players = false
+ESP3:Toggle(true); ESP3.Names = false; ESP3.Players = false; ESP3.FaceCamera = true
 
 local player = game.Players.LocalPlayer
 
+ESP3:AddObjectListener(workspace.World.Objectives, {
+    Color = Color3.fromRGB(255,204,0),
+    Type = 'Model',
+    PrimaryPart = function(obj) while not obj:FindFirstChildWhichIsA('Part') do wait() end return obj:FindFirstChildWhichIsA('Part') end,
+    IsEnabled = function(obj) return ESP2[obj.Name] and player:DistanceFromCharacter(obj.PrimaryPart.Position) < objectiveRange end
+})
 ESP2:AddObjectListener(workspace.Ignore.Items, {
     Color = Color3.fromRGB(102,255,102),
     Type = 'Model',
@@ -23,7 +31,7 @@ ESP:AddObjectListener(workspace.Entities.Infected, {
 do 
 	local Window = library:AddWindow("Those Who Remain ESP", {
 		main_color = Color3.fromRGB(41, 74, 122),
-		min_size = Vector2.new(400, 500),
+		min_size = Vector2.new(500, 600),
 		toggle_key = Enum.KeyCode.RightShift,
 		can_resize = true,
 	})
@@ -31,7 +39,9 @@ do
 	do
         visuals:AddSwitch("Player ESP", function(v) ESP.Players = v end)
 		visuals:AddSwitch("Zombie ESP", function(v) ESP.Zombies = v end)
-        local zombRangeSlider = visuals:AddSlider("Zombie Range", function(v) zombieRange = v end, {["min"] = 0, ["max"] = 10000, ["readonly"] = false})
+		visuals:AddSwitch("Objective ESP", function(v) ESP3.Objectives = v end)
+        local zombRangeSlider = visuals:AddSlider("Zombie Range", function(v) zombieRange = v end, {["min"] = 0, ["max"] = 2500, ["readonly"] = false})
+        local objectiveRangeSlider = visuals:AddSlider("Objective Range", function(v) objectiveRange = v end, {["min"] = 0, ["max"] = 1000, ["readonly"] = false})
 
         local itemEsp = visuals:AddFolder("Item ESP")
         local itemRangeSlider = itemEsp:AddSlider("Range", function(v) itemRange = v end, {["min"] = 0, ["max"] = 1000, ["readonly"] = false})
